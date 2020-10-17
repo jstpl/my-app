ac6e8692817a1f3d90b50bc9d8331b7f4830eed4
Vitaliy <theyamshikov@yandex.ru>
2020-10-18 01:54:53 +0600

    upd

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,5 @@
 import React, {Component} from 'react';
 import {Container} from 'bootstrap-4-react';
-import {Route, Switch} from 'react-router-dom'
-
-import Home from '../pages/site/components/home';
-import About from '../pages/site/components/about';
-import List from '../pages/user/components/list';
-import Profile from '../pages/user/components/profile';
 
 export default class Main extends Component {
     render() {

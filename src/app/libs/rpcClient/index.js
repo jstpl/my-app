17f8371872fc291e9279399919663a7696ee2665
Vitaliy <theyamshikov@yandex.ru>
2021-11-12 17:18:51 +0600

    upd

--- src/app/libs/rpcClient/index.js
+++ src/app/libs/rpcClient/index.js
@@ -1,3 +1,6 @@
-import RpcFactory from "./RpcFactory";
+import Client from "./Client";
+import Transport from "./Transport";
+import ResponseEncoder from "./encoders/ResponseEncoder";
+import RequestEncoder from "./encoders/RequestEncoder";
 
-export default RpcFactory.createClient();
+export default new Client(new Transport(), new RequestEncoder(), new ResponseEncoder());

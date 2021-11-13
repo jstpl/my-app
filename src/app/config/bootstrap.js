c564bd1b310d01336aa30f848f758cbec73412a2
Vitaliy <theyamshikov@yandex.ru>
2021-11-13 20:01:53 +0600

    upd

--- src/app/config/bootstrap.js
+++ src/app/config/bootstrap.js
@@ -2,10 +2,11 @@ import configManager from "../singletons/configManager";
 import config from "./config";
 import event from "./event";
 
-export default class Bootstrap {
+/*export default class Bootstrap {
 
     init() {
-        configManager.load(config);
-        event.init();
     }
-}
+}*/
+
+configManager.load(config);
+event.init();
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './app/config/store';
 import router from './app/config/router';
-import Bootstrap from "./app/config/bootstrap";
+// import Bootstrap from "./app/config/bootstrap";
+import "./app/config/bootstrap";
 
-let bootstrap = new Bootstrap();
-bootstrap.init();
+// (new Bootstrap()).init();
 
 ReactDOM.render(
     <Provider store={store}>{router}</Provider>,

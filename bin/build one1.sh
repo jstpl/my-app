8996355ed88870b0068da21c35dcfe6776c034cf
Vitaliy <theyamshikov@yandex.ru>
2021-11-20 21:14:05 +0600

    upd

---
+++ bin/build one1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd ..
+cd src/packages/pack
+node build-in-one.js
--- src/packages/pack/build-in-one.js
+++ src/packages/pack/build-in-one.js
@@ -2,10 +2,9 @@
 
 var fs = require('fs'),
     path = require('path');
-
+// const zlib = require('zlib');
 const jsdom = require("jsdom");
 const basePath = '/home/vitaliy/common/var/www/tpl/my-app/build';
-
 
 let data = fs.readFileSync(basePath + '/index.html', {encoding: 'utf-8'});
 
@@ -19,18 +18,35 @@ for(let i in linkElems) {
         let fileData = fs.readFileSync(filePath, {encoding: 'utf-8'});
         data = data.replace(elem.outerHTML, '<style>' + fileData + '</style>');
     }
+    if(elem.rel === 'shortcut icon') {
+        let filePath = basePath + '/' + elem.href;
+        let fileData = fs.readFileSync(filePath);
+        let tagCode = elem.outerHTML;
+        elem.href = 'data:image/x-icon;base64,' + Buffer.from(fileData).toString('base64');
+        data = data.replace(tagCode, elem.outerHTML);
+    }
+    if(elem.rel === 'manifest') {
+        let filePath = basePath + '/' + elem.href;
+        let fileData = fs.readFileSync(filePath);
+        let tagCode = elem.outerHTML;
+        elem.href = 'data:text/cache-manifest;base64,' + Buffer.from(fileData).toString('base64');
+        data = data.replace(tagCode, elem.outerHTML);
+    }
 }
 
 let scriptElems = dom.window.document.querySelectorAll("script");
 for(let i in scriptElems) {
     let elem = scriptElems[i];
-
     if(typeof elem.src !== 'undefined' && elem.innerHTML === '') {
         let filePath = basePath + '/' + elem.src;
         let fileData = fs.readFileSync(filePath, {encoding: 'utf-8'});
         let tagCode = elem.outerHTML;
+
+        //let buffer = zlib.gzipSync( fileData );
+
         elem.src = 'data:text/javascript;base64,' + Buffer.from(fileData).toString('base64');
         data = data.replace(tagCode, elem.outerHTML);
+
     }
 }
 

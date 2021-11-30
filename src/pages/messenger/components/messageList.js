da8e71fff9836300dbdae165c938ff924d7c9d3d
ZnDeveloper <zndev@ya.ru>
2021-11-30 22:35:06 +0600

    upd

--- src/pages/messenger/components/messageList.js
+++ src/pages/messenger/components/messageList.js
@@ -8,22 +8,15 @@ class MessageList extends Component {
 
     componentDidMount() {
         let userId = this.props.match.params.id;
-        console.log(userId);
         if(userId) {
             let query = new Query();
             query.filter.chatId = userId;
-            // console.log(userId);
             messageService.all(query).then(function () {});
         }
     }
 
     render() {
-        // this.update();
         return (
-            /*<>
-                aaa
-                {this.props.match.params.id}
-            </>*/
             <MessageListView dataProvider={this.props.dataProvider}/>
         );
     }

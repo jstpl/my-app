aa3f7090aa3b03239b37e199c6d2cc475a56288d
Vitaliy <theyamshikov@yandex.ru>
2021-11-30 17:26:22 +0600

    upd

--- src/pages/messenger/index.js
+++ src/pages/messenger/index.js
@@ -5,8 +5,10 @@ import CrudDispatcher from "../../packages/domain/dispatchers/CrudDispatcher";
 
 const actionPrefix = 'messengerChat';
 
-let storeDispatcher = new CrudDispatcher(actionPrefix);
-let chatService = new ChatService(storeDispatcher, new ChatRepository());
+let chatService = new ChatService(
+    new CrudDispatcher(actionPrefix),
+    new ChatRepository()
+);
 
 let chatReducer = new CrudReducer(actionPrefix);
 let chatState = chatReducer.getState();

88e592b373b2c0fdb95ee51e845fdf9bb03380fa
Vitaliy <theyamshikov@yandex.ru>
2021-11-30 15:23:32 +0600

    upd

--- src/app/config/store.js
+++ src/app/config/store.js
@@ -3,11 +3,11 @@ import {combineReducers, createStore} from 'redux';
 // Reducers
 import userReducer from '../../pages/user/reducers/userReducer';
 import authReducer from '../../pages/auth/reducers/authReducer';
-import {chatReducer} from "../../pages/messenger";
+import {chatState} from "../../pages/messenger";
 
 // Combine Reducers
 let reducers = combineReducers({
-    chatState: chatReducer,
+    chatState: chatState,
     userState: userReducer,
     authState: authReducer,
 });
--- src/pages/messenger/index.js
+++ src/pages/messenger/index.js
@@ -1,37 +1,13 @@
 import ChatRepository from "./repositories/rpc/ChatRepository";
 import ChatService from "./services/ChatService";
-// import ChatDispatcher from "./dispatchers/ChatDispatcher";
 import CrudReducer from "../../packages/domain/reducers/CrudReducer";
 import CrudDispatcher from "../../packages/domain/reducers/CrudDispatcher";
-// import store from "../../app/config/store";
-// import DataProvider from "../../packages/domain/DataProvider";
 
 const actionPrefix = 'messengerChat';
 
-/**
- * можно создавать кастомные редьюсеры, наследуя их от BaseCrudReducer
- */
-/*let chatReducerInstance = new CrudReducer();
-chatReducerInstance.prefix = actionPrefix;
-chatReducerInstance.initialState = {
-    dataProvider: new DataProvider(),
-};*/
-
-// let chatReducer = chatReducerInstance.run.bind(chatReducerInstance);
-// let chatReducer = chatReducerInstance.getReducer();
-
-
 let chatService = new ChatService(new ChatRepository());
-// chatService.reducer =
 chatService.reducerDispatcher = new CrudDispatcher(actionPrefix);
-let chatReducer = (new CrudReducer(actionPrefix)).getState();
-// chatService.reducerPrefix = actionPrefix;
+let chatReducer = new CrudReducer(actionPrefix);
+let chatState = chatReducer.getState();
 
-// let dispatcher = new ChatDispatcher();
-// dispatcher.reducerPrefix = actionPrefix;
-// chatService.reducerDispatcher = dispatcher;
-
-// chatService.reducer = chatReducerInstance;
-
-
-export {chatService, chatReducer};
+export {chatService, chatState};

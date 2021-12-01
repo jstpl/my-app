e635f75fb2dde53165b1a9f49daae11e1c260dba
ZnDeveloper <zndev@ya.ru>
2021-12-02 00:59:42 +0600

    upd

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './app/config/store';
 import router from './app/config/router';
-import "./app/config/bootstrap";
 import domain from "./app/config/domain";
+import "./app/config/bootstrap";
 
 domain.app.store = store;
 

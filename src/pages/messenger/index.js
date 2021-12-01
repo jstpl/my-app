5ea9d85543b3e74d232ec762189f729b65ded67e
ZnDeveloper <zndev@ya.ru>
2021-12-01 16:27:34 +0600

    upd

--- src/pages/messenger/index.js
+++ src/pages/messenger/index.js
@@ -1,12 +1,6 @@
-// import ChatRepository from "./repositories/rpc/ChatRepository";
-// import ChatService from "./services/ChatService";
 import CrudReducer from "../../packages/domain/reducers/CrudReducer";
-import CrudDispatcher from "../../packages/domain/dispatchers/CrudDispatcher";
-import BaseCrudService from "../../packages/domain/services/BaseCrudService";
 import CrudRpcRepository from "../../packages/rpc/CrudRpcRepository";
-import ChatDispatcher from "./dispatchers/ChatDispatcher";
 import MessageService from "./services/MessageService";
-import MessageDispatcher from "./dispatchers/MessageDispatcher";
 import ChatService from "./services/ChatService";
 import messengerReducerPrefix from "./enums/messengerReducerPrefix";
 import dispatchers from "./config/dispatchers";

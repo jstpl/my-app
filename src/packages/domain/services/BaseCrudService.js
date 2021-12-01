571b84d1f857ad6ab46d93174ab2b1e6ae00c59d
ZnDeveloper <zndev@ya.ru>
2021-12-01 20:22:34 +0600

    upd

--- src/packages/domain/services/BaseCrudService.js
+++ src/packages/domain/services/BaseCrudService.js
@@ -1,30 +1,28 @@
 import BaseService from "./BaseService";
-import Query from "../libs/Query";
 
 export default class BaseCrudService extends BaseService {
 
-    #_storeDispatcher = null;
+    #_storeRepository = null;
     #_repository = null;
 
     constructor(storeDispatcher = null, repository) {
         super();
-        this.#_storeDispatcher = storeDispatcher;
+        this.#_storeRepository = storeDispatcher;
         this.#_repository = repository;
-        this._repository = repository;
     }
 
-    get storeDispatcher() {
-        return this.#_storeDispatcher;
+    get storeRepository() {
+        return this.#_storeRepository;
     }
 
     get repository() {
-        return this._repository;
+        return this.#_repository;
     }
 
     async all(query = null) {
         try {
             let dataProvider = await this.repository.all(query);
-            this.storeDispatcher.setDataProvider(dataProvider);
+            this.storeRepository.setDataProvider(dataProvider);
         } catch (error) {
             throw error;
         }

fc2cf25d9b2ab58e717019e3c2d8559d798387f5
ZnDeveloper <zndev@ya.ru>
2021-12-02 23:10:39 +0600

    upd

--- src/app/config/bootstrap.js
+++ src/app/config/bootstrap.js
@@ -1,9 +1,5 @@
-
 import config from "./config";
 import "./event";
 import configManager from "../../packages/configManager/configManager";
-import container from "./container";
 
 configManager.load(config);
-
-container.security.services.security.init();

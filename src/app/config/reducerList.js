f22b6ffe01d1d15b62c4d1a4ecad0a7cf89a99c2
Vitaliy <theyamshikov@yandex.ru>
2021-12-02 10:01:39 +0600

    upd

--- src/app/config/reducerList.js
+++
@@ -1,8 +0,0 @@
-import domain from "./domain";
-import reduceHelper from "../../packages/domain/helpers/reduceHelper";
-
-let reducers = {};
-let reducersFromDomains = reduceHelper.getReducersFromDomains(domain);
-Object.assign(reducers, reducersFromDomains);
-
-export default reducers;
--- src/app/config/store.js
+++ src/app/config/store.js
@@ -1,6 +1,8 @@
 import {combineReducers, createStore} from 'redux';
-import reducerList from "./reducerList";
+import reduceHelper from "../../packages/domain/helpers/reduceHelper";
+import domain from "./domain";
 
-let reducers = combineReducers(reducerList);
+let domainReducers = reduceHelper.getReducersFromDomains(domain);
+let reducers = combineReducers(domainReducers);
 const store = createStore(reducers);
 export default store;

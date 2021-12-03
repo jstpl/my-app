127a8caa536173960303ee961253266f2b87bd6c
ZnDeveloper <zndev@ya.ru>
2021-12-03 22:59:05 +0600

    upd

--- src/pages/messenger/web/components/messageList.js
+++ src/pages/messenger/web/components/messageList.js
@@ -1,38 +1,24 @@
 import React, {Component} from 'react';
-import MessageListView from '../views/messageList'
+// import MessageListView from '../views/messageList'
 import {connect} from "react-redux";
 import Query from "../../../../packages/domain/libs/Query";
 import container from "../../../../app/config/container";
+import MessageForm from "./messageForm";
 
 class MessageList extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            login: '',
-            password: '',
-            errors: {},
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event) {
-        let name = event.target.name;
-        let state = {};
-        state[name] = event.target.value;
-        this.setState(state);
-    }
 
     updateList() {
         let chatId = this.props.match.params.chatId;
         let query = new Query();
         query.filter.chatId = chatId;
-        container.messenger.services.message.all(query).catch(function () {
+        container.messenger.services.message.all(query).then(() => {
+            //this.scrollToBottom();
         });
     }
 
     componentDidMount() {
         this.updateList();
+        this.scrollToBottom();
     }
 
     componentDidUpdate(prevProps) {
@@ -42,11 +28,58 @@ class MessageList extends Component {
         if (chatId !== prevChatId) {
             this.updateList();
         }
+        this.scrollToBottom();
+    }
+
+    scrollToBottom() {
+        this.messagesList.scrollTop = this.messagesList.scrollHeight;
     }
 
     render() {
         return (
-            <MessageListView dataProvider={this.props.dataProvider}/>
+            <div className="data-list">
+                <div className="card card-primary direct-chat direct-chat-primary">
+                    <div className="card-header">
+                        <h3 className="card-title">Direct Chat</h3>
+                        <div className="card-tools">
+                            <span data-toggle="tooltip" title="3 New Messages" className="badge badge-light">3</span>
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <div className="direct-chat-messages" ref={(el) => { this.messagesList = el; }}>>
+
+                            {this.props.dataProvider && this.props.dataProvider.collection ? (
+                                this.props.dataProvider.collection.map(function (messageEntity) {
+                                    return (
+                                        <div key={messageEntity.id} className="direct-chat-msg">
+                                            <div className="direct-chat-infos clearfix">
+                                                <span className="direct-chat-name float-left">{messageEntity.author.username}</span>
+                                                <span className="direct-chat-timestamp float-right">{messageEntity.createdAt}</span>
+                                            </div>
+                                            <img className="direct-chat-img" src={messageEntity.author.logo}
+                                                 alt="message user image"/>
+                                            <div className="direct-chat-text">
+                                                {messageEntity.text}
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            ) : (
+                                <div>Empty</div>
+                            )}
+
+                            <div style={{ float:"left", clear: "both" }}
+                                 ref={(el) => { this.messagesEnd = el; }}>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="card-footer">
+                        <MessageForm/>
+                    </div>
+                </div>
+            </div>
+            // <MessageListView messagesEnd={this.messagesEnd} dataProvider={this.props.dataProvider}/>
         );
     }
 }

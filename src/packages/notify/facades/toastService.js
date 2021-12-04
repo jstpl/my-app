bfe9970cf6d0594510170523586cf037f8ba5f68
ZnDeveloper <zndev@ya.ru>
2021-12-04 14:17:52 +0600

    upd

--- src/packages/notify/facades/toastService.js
+++ src/packages/notify/facades/toastService.js
@@ -1,33 +1,34 @@
 import container from "../../../app/config/container";
 import ToastEntity from "../entities/ToastEntity";
 
-let toastService = container.notify.services.toast;
+let service = container.notify.services.toast;
 
-class ToastServiceFacade {
+class ToastFacade {
 
     success(message, position = null) {
         let toastEntity = new ToastEntity();
         toastEntity.message = message;
         toastEntity.position = position;
-        toastEntity.type = toastService.types.SUCCESS;
-        toastService.send(toastEntity);
+        toastEntity.type = service.types.SUCCESS;
+        service.send(toastEntity);
     }
 
     info(message, position = null) {
         let toastEntity = new ToastEntity();
         toastEntity.message = message;
         toastEntity.position = position;
-        toastEntity.type = toastService.types.INFO;
-        toastService.send(toastEntity);
+        toastEntity.type = service.types.INFO;
+        service.send(toastEntity);
     }
 
     error(message, position = null) {
         let toastEntity = new ToastEntity();
         toastEntity.message = message;
         toastEntity.position = position;
-        toastEntity.type = toastService.types.ERROR;
-        toastService.send(toastEntity);
+        toastEntity.type = service.types.ERROR;
+        service.send(toastEntity);
     }
 }
 
-export default new ToastServiceFacade;
+let toastService = new ToastFacade();
+export default toastService;
